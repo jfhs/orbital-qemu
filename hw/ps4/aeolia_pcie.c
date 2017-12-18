0e038d86d1c7c8d2ceb0c19d6e1cd0383d4308f3
Alexandro Sanchez Bach <alexandro@phi.nz>
2017-12-18 09:15:15 -0800

    Fixed Orbis hdac0 and apcie0 mapping issues

--- hw/ps4/aeolia_pcie.c
+++ hw/ps4/aeolia_pcie.c
@@ -21,13 +21,138 @@
 #include "qemu/osdep.h"
 #include "hw/pci/pci.h"
 
+// MMIO
+#define APCIE_CHIP_ID0 0x1104
+#define APCIE_CHIP_ID1 0x1108
+#define APCIE_CHIP_REV 0x110C
+
+#define HPET_UNK004 0x0004
+#define HPET_UNK010 0x0010
+
+// Peripherals
+#define HPET_BASE 0x182000
+#define HPET_SIZE 0x1000
+#define CONTAINS(peripheral, addr) \
+    (peripheral##_BASE <= addr && addr < peripheral##_BASE + peripheral##_SIZE)
+
+// Helpers
+#define AEOLIA_PCIE(obj) \
+    OBJECT_CHECK(AeoliaPCIEState, (obj), TYPE_AEOLIA_PCIE)
+
 typedef struct AeoliaPCIEState {
+    /*< private >*/
     PCIDevice parent_obj;
+    /*< public >*/
+    MemoryRegion iomem[3];
+
 } AeoliaPCIEState;
 
-static int aeolia_pcie_init(PCIDevice *dev)
+/* Aeolia PCIe Unk0 */
+static uint64_t aeolia_pcie_0_read
+    (void *opaque, hwaddr addr, unsigned size)
 {
+    printf("aeolia_pcie_0_read:  { addr: %lX, size: %X }\n", addr, size);
     return 0;
+}
+
+static void aeolia_pcie_0_write
+    (void *opaque, hwaddr addr, uint64_t value, unsigned size)
+{
+    printf("aeolia_pcie_0_write: { addr: %lX, size: %X, value: %lX }\n", addr, size, value);
+}
+
+static const MemoryRegionOps aeolia_pcie_0_ops = {
+    .read = aeolia_pcie_0_read,
+    .write = aeolia_pcie_0_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+};
+
+/* Aeolia PCIe Unk1 */
+static uint64_t aeolia_pcie_1_read
+    (void *opaque, hwaddr addr, unsigned size)
+{
+    switch (addr) {
+    case APCIE_CHIP_ID0:
+        return 0x41B30130;
+    case APCIE_CHIP_ID1:
+        return 0x52024D44;
+    case APCIE_CHIP_REV:
+        return 0x00000300;
+    }
+    printf("aeolia_pcie_1_read:  { addr: %lX, size: %X }\n", addr, size);
+    return 0;
+}
+
+static void aeolia_pcie_1_write
+    (void *opaque, hwaddr addr, uint64_t value, unsigned size)
+{
+    printf("aeolia_pcie_1_write: { addr: %lX, size: %X, value: %lX }\n", addr, size, value);
+}
+
+static const MemoryRegionOps aeolia_pcie_1_ops = {
+    .read = aeolia_pcie_1_read,
+    .write = aeolia_pcie_1_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+};
+
+/* Aeolia PCIe Peripherals */
+static uint64_t aeolia_pcie_peripherals_read(
+    void *opaque, hwaddr addr, unsigned size)
+{
+    //AeoliaPCIEState *s = opaque;
+
+    if (CONTAINS(HPET, addr)) {
+        addr -= HPET_BASE;
+        printf("aeolia_hpet_read:  { addr: %lX, size: %X }\n", addr, size);
+        switch (addr) {
+        case HPET_UNK004:
+            return 0xFFFFFFFF;
+        case HPET_UNK010:
+            return 0;
+        }
+        return 0;
+    }
+    else {
+        printf("aeolia_pcie_peripherals_read:  { addr: %lX, size: %X }\n", addr, size);
+        return 0;
+    }
+}
+
+static void aeolia_pcie_peripherals_write(
+    void *opaque, hwaddr addr, uint64_t value, unsigned size)
+{
+    //AeoliaPCIEState *s = opaque;
+
+    printf("aeolia_pcie_peripherals_write: { addr: %lX, size: %X, value: %lX }\n", addr, size, value);
+}
+
+static const MemoryRegionOps aeolia_pcie_peripherals_ops = {
+    .read = aeolia_pcie_peripherals_read,
+    .write = aeolia_pcie_peripherals_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+};
+
+static void aeolia_pcie_realize(PCIDevice *dev, Error **errp)
+{
+    AeoliaPCIEState *s = AEOLIA_PCIE(dev);
+
+    // PCI Configuration Space
+    dev->config[PCI_CLASS_PROG] = 0x04;
+    dev->config[PCI_INTERRUPT_LINE] = 0xFF;
+    dev->config[PCI_INTERRUPT_PIN] = 0x00;
+    pci_add_capability(dev, PCI_CAP_ID_MSI, 0, PCI_CAP_SIZEOF, errp);
+
+    // Memory
+    memory_region_init_io(&s->iomem[0], OBJECT(dev),
+        &aeolia_pcie_0_ops, s, "aeolia-pcie-0", 0x100000);
+    memory_region_init_io(&s->iomem[1], OBJECT(dev),
+        &aeolia_pcie_1_ops, s, "aeolia-pcie-1", 0x8000);
+    memory_region_init_io(&s->iomem[2], OBJECT(dev),
+        &aeolia_pcie_peripherals_ops, s, "aeolia-pcie-peripherals", 0x200000);
+
+    pci_register_bar(dev, 0, PCI_BASE_ADDRESS_SPACE_MEMORY, &s->iomem[0]);
+    pci_register_bar(dev, 2, PCI_BASE_ADDRESS_SPACE_MEMORY, &s->iomem[1]);
+    pci_register_bar(dev, 4, PCI_BASE_ADDRESS_SPACE_MEMORY, &s->iomem[2]);
 }
 
 static void aeolia_pcie_class_init(ObjectClass *klass, void *data)
@@ -36,10 +161,10 @@ static void aeolia_pcie_class_init(ObjectClass *klass, void *data)
 
     pc->vendor_id = 0x104D;
     pc->device_id = 0x90A1;
-    pc->revision = 1;
+    pc->revision = 0;
     pc->is_express = true;
-    pc->class_id = PCI_CLASS_STORAGE_RAID;
-    pc->init = aeolia_pcie_init;
+    pc->class_id = PCI_CLASS_SYSTEM_OTHER;
+    pc->realize = aeolia_pcie_realize;
 }
 
 static const TypeInfo aeolia_pcie_info = {
--- hw/ps4/liverpool_hdac.c
+++ hw/ps4/liverpool_hdac.c
@@ -20,14 +20,60 @@
 #include "liverpool.h"
 #include "qemu/osdep.h"
 #include "hw/pci/pci.h"
+#include "macros.h"
+
+// Helpers
+#define MMIO_R(...) MMIO_READ(s->mmio, __VA_ARGS__)
+#define MMIO_W(...) MMIO_WRITE(s->mmio, __VA_ARGS__)
+
+#define LIVERPOOL_HDAC(obj) \
+    OBJECT_CHECK(LiverpoolHDACState, (obj), TYPE_LIVERPOOL_HDAC)
 
 typedef struct LiverpoolHDACState {
+    /*< private >*/
     PCIDevice parent_obj;
+    /*< public >*/
+    MemoryRegion iomem;
+    uint32_t mmio[0x1000];
 } LiverpoolHDACState;
 
-static int liverpool_hdac_init(PCIDevice *dev)
+static uint64_t liverpool_hdac_read
+    (void *opaque, hwaddr addr, unsigned size)
 {
-    return 0;
+    LiverpoolHDACState *s = opaque;
+
+    printf("liverpool_hdac_read:  { addr: %lX, size: %X }\n", addr, size);
+    return MMIO_R(addr);
+}
+
+static void liverpool_hdac_write
+    (void *opaque, hwaddr addr, uint64_t value, unsigned size)
+{
+    LiverpoolHDACState *s = opaque;
+
+    MMIO_W(addr, value);
+    printf("liverpool_hdac_write: { addr: %lX, size: %X, value: %lX }\n", addr, size, value);
+}
+
+static const MemoryRegionOps liverpool_hdac_ops = {
+    .read = liverpool_hdac_read,
+    .write = liverpool_hdac_write,
+    .endianness = DEVICE_LITTLE_ENDIAN,
+};
+
+static void liverpool_hdac_realize(PCIDevice *dev, Error **errp)
+{
+    LiverpoolHDACState *s = LIVERPOOL_HDAC(dev);
+
+    // PCI Configuration Space
+    dev->config[PCI_INTERRUPT_LINE] = 0xFF;
+    dev->config[PCI_INTERRUPT_PIN] = 0x02;
+    pci_add_capability(dev, PCI_CAP_ID_MSI, 0, PCI_CAP_SIZEOF, errp);
+
+    // Memory
+    memory_region_init_io(&s->iomem, OBJECT(dev),
+        &liverpool_hdac_ops, s, "liverpool-hdac-0", 0x4000);
+    pci_register_bar(dev, 0, PCI_BASE_ADDRESS_SPACE_MEMORY, &s->iomem);
 }
 
 static void liverpool_hdac_class_init(ObjectClass *klass, void *data)
@@ -36,10 +82,10 @@ static void liverpool_hdac_class_init(ObjectClass *klass, void *data)
 
     pc->vendor_id = 0x1002;
     pc->device_id = 0x9921;
-    pc->revision = 1;
+    pc->revision = 0;
     pc->is_express = true;
     pc->class_id = PCI_CLASS_MULTIMEDIA_AUDIO;
-    pc->init = liverpool_hdac_init;
+    pc->realize = liverpool_hdac_realize;
 }
 
 static const TypeInfo liverpool_hdac_info = {
